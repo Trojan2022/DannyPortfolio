f38b04572d29310681c9c016b95e01f70f14243f
Trojan <trojan22yrs@gmail.com>
2022-10-31 20:19:00 +0630

    my mordern portfolio

---
+++ typing.js
@@ -0,0 +1,8 @@
+//typing with cdnjs.com
+
+var typed = new typed('.typing', {
+    strings: ['', 'Web Designer', 'Web Developer', 'Graphic Designer', 'YouTuber'],
+    typeSpeed: 100,
+    Backspeed: 60,
+    loop: true
+})
